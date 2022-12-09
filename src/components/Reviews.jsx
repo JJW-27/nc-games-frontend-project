67c47b7cff64a3415492c82d2c8bb3c5ffff6d16
JJW-27 <josephjonwilliams@outlook.com>
2022-12-09 14:00:20 +0000

    adds review posted date, and begins sorting navigation bar

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -19,28 +19,37 @@ const Reviews = () => {
   return reviewsIsLoading ? (
     <h2 className="loading">Loading...</h2>
   ) : (
-    <ul className="all-reviews-list">
-      {reviews.map(review => {
-        return (
-          <li className="single-review-list-item" key={review.review_id}>
-            <h2>{review.title}</h2>
-            <p>
-              Reviewed by: <em>{review.owner}</em>
-            </p>
-            <img src={review.review_img_url} alt={review.title} />
-            <p>
-              <em>{review.review_body.slice(0, 100)} </em>
-              <Link to={`/review/${review.review_id}`}>
-                <em>...</em>
-                <strong>read more</strong>
-              </Link>
-            </p>
-            <p>votes: {review.votes}</p>
-            <p>comments: {review.comment_count}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="reviews-container">
+      <nav>
+        Sort by:<Link>Date (asc)</Link>
+        <Link>Date (desc)</Link>
+      </nav>
+      <ul className="all-reviews-list">
+        {reviews.map(review => {
+          return (
+            <li className="single-review-list-item" key={review.review_id}>
+              <h2>{review.title}</h2>
+              <p>
+                Reviewed by: <em>{review.owner}</em>
+              </p>
+              <img src={review.review_img_url} alt={review.title} />
+              <p>
+                <em>{review.review_body.slice(0, 100)} </em>
+                <Link to={`/review/${review.review_id}`}>
+                  <em>...</em>
+                  <strong>read more</strong>
+                </Link>
+              </p>
+              <p>votes: {review.votes}</p>
+              <p>comments: {review.comment_count}</p>
+              <p>
+                <em>Posted: {new Date(review.created_at).toLocaleString()}</em>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
